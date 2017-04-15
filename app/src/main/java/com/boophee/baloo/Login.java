97b0ebdf82d7340ce573dca5afbd76aa5a2bc276
uday singh <f71uday@gmail.com>
2017-04-15 14:11:50 +0530

    login view created and view were linked in login.java

--- app/src/main/java/com/boophee/baloo/Login.java
+++ app/src/main/java/com/boophee/baloo/Login.java
@@ -2,6 +2,9 @@ package com.boophee.baloo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText editText_email;
+        EditText editText_password;
+        Button button_signup;
+        ProgressBar progressBar_signup;
+        editText_email= (EditText)findViewById(R.id.editText_email);
+        editText_password=(EditText)findViewById(R.id.editText_password);
+        button_signup=(Button)findViewById(R.id.button_signup);
+        progressBar_signup= (ProgressBar)findViewById(R.id.progressBar_signup);
+
     }
 }
